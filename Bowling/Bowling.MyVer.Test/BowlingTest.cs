1804fc5a443fc0f84f0739d958fdc940a4be7323
ojidev100days <ojidev100days@gmail.com>
2022-04-15 12:32:57 +0900

    bowling add-test フレームが加算されるテストを追加

--- Bowling/Bowling.MyVer.Test/BowlingTest.cs
+++ Bowling/Bowling.MyVer.Test/BowlingTest.cs
@@ -8,14 +8,17 @@ namespace Bowling.MyVer.Test
     /*
      - [ ] 倒したピンを追加する
        - [ ] 倒したピンはスコアに合算される
-         * 初期状態のスコアは0
-         * 倒したピンが0の時スコアは0を返す
-         * 倒したピンが1の時スコアは1を返す 
-         * 倒したピンが1と2の時スコアは3を返す 
-       - [ ] ピンの数は0から10の範囲で追加する。
-         * [ ] 
-       - [ ] ピンは10フレームが完了するまで追加できる
-       - [ ] 1フレームに2回ピンの数を追加できる。ただし例外あり。
+         * [x] 初期状態のスコアは0
+         * [x] 倒したピンが0の時スコアは0を返す
+         * [x] 倒したピンが1の時スコアは1を返す 
+         * [x] 倒したピンが1と2の時スコアは3を返す 
+       - [ ] 1回で追加できるピンの数は0から10
+       - [ ] 1フレームに2回ピンの数を追加できる
+         * [x] 初期状態のフレームNoは1
+         * [x] 2回ピンを追加するとフレームNoは2
+         * [ ] 4回ピンを追加するとフレームNoは3
+         * TODO ストライク周りの話が出てきそう
+     - [ ] ピンは10フレームが完了するまで追加できる
          - [ ] 1フレームで追加で切るピンの数は、2回合わせて0から10。
          - [ ] 1フレームの1回目のピンの追加でピンの数が10の場合、2回目のピンの数の追加はできない（ストライク。次のフレームに移る）
          - [ ] 10フレームのみの例外あり
@@ -36,7 +39,7 @@ namespace Bowling.MyVer.Test
             private readonly Game _target = new Game();
 
             [Fact]
-            public void 初期状態のスコアは0()
+            internal void 初期状態のスコアは0()
             {
                 // Given
                 // When
@@ -46,7 +49,7 @@ namespace Bowling.MyVer.Test
             }
 
             [Fact]
-            public void 倒したピンが0の時スコアは0を返す()
+            internal void 倒したピンが0の時スコアは0を返す()
             {
                 // Given
                 // When
@@ -58,7 +61,7 @@ namespace Bowling.MyVer.Test
             }
 
             [Fact]
-            public void 倒したピンが1の時スコアは1を返す()
+            internal void 倒したピンが1の時スコアは1を返す()
             {
                 // Given
                 // When
@@ -70,7 +73,7 @@ namespace Bowling.MyVer.Test
             }
 
             [Fact]
-            public void 倒したピンが1と2の時スコアは3を返す()
+            internal void 倒したピンが1と2の時スコアは3を返す()
             {
                 // Given
                 // When
@@ -82,6 +85,52 @@ namespace Bowling.MyVer.Test
             }
 
         }
+
+
+        public class _1フレームに2回ピンの数を追加できる
+        {
+            private readonly Game _target = new Game();
+
+            [Fact]
+            internal void 初期状態のフレームNoは1()
+            {
+                // Given
+                // When
+                // Then
+                _target.Frame.Should().Be(1);
+
+            }
+
+            [Fact]
+            internal void _2回ピンを追加するとフレームNoは2()
+            {
+                // Given
+                // When
+                _target.Add(1);
+                _target.Add(2);
+
+                // Then
+                _target.Frame.Should().Be(2);
+
+            }
+
+
+            [Fact]
+            internal void _4回ピンを追加するとフレームNoが3()
+            {
+                // Given
+                // When
+                _target.Add(1);
+                _target.Add(2);
+                _target.Add(3);
+                _target.Add(4);
+
+                // Then
+                _target.Frame.Should().Be(3);
+
+            }
+        }
+
 
     }
 
--- Bowling/Bowling.MyVer/Game.cs
+++ Bowling/Bowling.MyVer/Game.cs
@@ -4,9 +4,14 @@ internal class Game
 {
 
     public int Score { get; private set; } = 0;
+    public int Frame { get; internal set; } = 1;
+
+    private int _addCount = 0;
 
     internal void Add(int pin)
     {
         Score += pin;
+        ;
+        if ((_addCount = ++_addCount % 2) == 0) Frame++;
     }
 }
