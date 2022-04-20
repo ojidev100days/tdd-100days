edc2719c98643df18337ad6ca944b3c837ab8636
ojidev100days <ojidev100days@gmail.com>
2022-04-20 12:35:26 +0900

    bowling 1回目にピンを10倒した場合のカレントフレームの計算方法を追加

--- Bowling/Bowling.MyVer.Test/BowlingTest.cs
+++ Bowling/Bowling.MyVer.Test/BowlingTest.cs
@@ -12,20 +12,26 @@ namespace Bowling.MyVer.Test
          * [x] 倒したピンが0の時スコアは0を返す
          * [x] 倒したピンが1の時スコアは1を返す 
          * [x] 倒したピンが1と2の時スコアは3を返す 
-       - [ ] 1回で追加できるピンの数は0から10
+       - [x] 1回で追加できるピンの数は0から10
          * [x] マイナス値を追加すると例外発生
-         * [ ] 0から10の値を追加すると正常終了
-         * [ ] 11以上の値を追加すると例外発生
+         * [x] 0から10の値を追加すると正常終了
+         * [x] 11以上の値を追加すると例外発生
        - [ ] 1フレームに2回ピンの数を追加できる
          * [x] 初期状態のフレームNoは1
          * [x] 2回ピンを追加するとフレームNoは2
          * [x] 4回ピンを追加するとフレームNoは3
-         * [ ] TODO ストライク周りの話が出てきそう
-       - [ ] 1から9フレームは1フレームで追加できるピンの合計数は0から10
+         * [x] フレームの最初でピンを10倒すとフレームは次に進む
+       - [ ] 1フレームで追加できるピンの合計数
+         - [x] 1から9フレームはピンの合計数は10まで
+           * [x] 1と10の組み合わせで追加すると例外発生
+           * [x] 9と2の組み合わせで追加すると例外発生
+       - [ ] 10フレームのみの例外
+         - [ ] フレームの最初で倒したピンの数が10の場合もう2回ピンを追加できる
+         - [ ] フレームの内で倒したピンの合計が10の場合もう1回ピンを追加できる
+
          * TODO
 
        - 仕様候補
-         - [ ] 1フレームの1回目のピンの追加でピンの数が10の場合、2回目のピンの数の追加はできない（ストライク。次のフレームに移る）
          - [ ] 10フレームのみの例外あり
            - [ ] ※後で追記
      - [ ] 現時点のフレームとそのフレームのスコアを表示する。
@@ -134,6 +140,21 @@ namespace Bowling.MyVer.Test
                 _target.Frame.Should().Be(3);
 
             }
+
+            [Theory]
+            [InlineData(new int[] { 10 }, 2)]
+            [InlineData(new int[] { 10, 10 }, 3)]
+            internal void フレームの最初でピンを10倒すとフレームは次に進む(int[] pins, int expectedFrameNo)
+            {
+                // Given
+                // When
+                foreach (var pin in pins) _target.Add(pin);
+
+                // Then
+                _target.Frame.Should().Be(expectedFrameNo);
+
+            }
+
         }
 
 
@@ -179,12 +200,42 @@ namespace Bowling.MyVer.Test
 
         }
 
-        public class _1から9フレームは1フレームで追加できるピンの合計数は0から10
-        {
-            private readonly Game _target = new Game();
 
-            // TODO
+
+        namespace _1フレームで追加できるピンの合計数
+        {
+
+            public class _1から9フレームは追加できるピンの合計数は10まで
+            {
+                private readonly Game _target = new Game();
+
+                [Fact]
+                internal void _1と10の組み合わせで追加すると例外発生()
+                {
+                    // Given
+                    // When
+                    _target.Add(1);
+                    var act = () => _target.Add(10);
+
+                    // Then
+                    act.Should().Throw<BowlingAppException>().WithMessage("The total number of pins that can be added in a frame is limited to 10.(currentFrame=0, frame=1, pin=10)");
+                }
+
+                [Fact]
+                internal void _9と2の組み合わせで追加すると例外発生()
+                {
+                    // Given
+                    // When
+                    _target.Add(9);
+                    var act = () => _target.Add(2);
+
+                    // Then
+                    act.Should().Throw<BowlingAppException>().WithMessage("The total number of pins that can be added in a frame is limited to 10.(currentFrame=0, frame=9, pin=2)");
+                }
+            }
+
         }
+
     }
 
     // TODO 今テストが書けないので後で
--- Bowling/Bowling.MyVer/Game.cs
+++ Bowling/Bowling.MyVer/Game.cs
@@ -4,15 +4,29 @@ internal class Game
 {
 
     public int Score { get; private set; } = 0;
-    public int Frame { get; internal set; } = 1;
+    public int Frame => _currentFrame + 1;
 
     private int _addCount = 0;
+
+    private int _currentFrame = 0;
+
+    private int[] _frames = new int[10];
 
     internal void Add(int pin)
     {
         if (pin < 0 || 10 < pin) throw new BowlingAppException($"The number of pins that can be added at one time is 0-10.(pin={pin})");
+        if (10 < _frames[_currentFrame] + pin) throw new BowlingAppException($"The total number of pins that can be added in a frame is limited to 10.(currentFrame={_currentFrame}, frame={_frames[_currentFrame]}, pin={pin})");
 
+        _frames[_currentFrame] += pin;
         Score += pin;
-        if ((_addCount = ++_addCount % 2) == 0) Frame++;
+
+        // ピンがストライクの場合
+        if (pin == 10)
+        {
+            _addCount = 0;
+            _currentFrame++;
+            return;
+        }
+        if ((_addCount = ++_addCount % 2) == 0) _currentFrame++;
     }
 }
